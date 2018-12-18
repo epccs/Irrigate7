15c2f72510f7edae2ab51aecf34f38ebe39a4564
ron sutherland <ronald.sutherland@gmail.com>
2018-12-18 00:45:06 -0700

    Digital: for ^5

---
+++ Digital/README.md
@@ -0,0 +1,102 @@
+# Digital Input/Output
+
+## Overview
+
+Digital is an interactive command line program that demonstrates control of the Digital input/output from ATmega1284p pins PD2,PD3 (2,3) and PA0..PA3 (24..27).
+
+## Firmware Upload
+
+With a serial port connection (set the BOOTLOAD_PORT in Makefile) and optiboot installed on the RPUno run 'make bootload' and it should compile and then flash the MCU.
+
+``` 
+sudo apt-get install git gcc-avr binutils-avr gdb-avr avr-libc avrdude
+git clone https://github.com/epccs/Irrigate7/
+cd /Irrigate7/Digital
+make bootload
+...
+avrdude done.  Thank you.
+``` 
+
+Now connect with picocom (or ilk).
+
+``` 
+#exit is C-a, C-x
+picocom -b 38400 /dev/ttyUSB0
+``` 
+
+
+# Commands
+
+Commands are interactive over the serial interface at 38400 baud rate. The echo will start after the second character of a new line. 
+
+
+## /\[rpu_address\]/\[command \[arg\]\]
+
+rpu_address is taken from the I2C address 0x29 (e.g. ../Uart/id.h get_Rpu_address() ). The value of rpu_address is used as a character in a string, which means don't use a null value (C strings are null terminated), but the ASCII value for '1' (0x31) is easy and looks nice, though I fear it will cause some confusion when it is discovered that the actual address value is 49.
+
+The STATUS_LED is bliked fast (twice per second) if the I2C address is not found, also the rpu_address defaults to '0'. 
+
+Commands and their arguments follow.
+
+
+## /0/id? \[name|desc|avr-gcc\]
+
+Identify is from ../Uart/id.h Id().
+
+``` 
+/1/id?
+{"id":{"name":"Digital","desc":"Irrigate7 (14320^5) Board /w atmega1284p","avr-gcc":"5.4.0"}}
+```
+
+##  /0/pMod 2|3|24..27,INPUT|OUTPUT    
+
+Set the Data Direction Register (DDRx) bit that sets a pin as INPUT or OUTPUT.
+
+``` 
+/1/pMod 2,OUTPUT
+{"PD2":"OUTPUT"}
+/1/pMod 3,INPUT
+{"PD3":"INPUT"}
+```
+
+Note: digital 3 is driven as a status LED and will stop blinking as an INPUT
+
+
+##  /0/dWrt 2|3|24..27,HIGH|LOW    
+
+Set the Port Data Register (PORTx) bit that drives the pin or if mode (e.g. Port Input Register bit) is set as an INPUT enables a pullup. Returns the Port Input Register PINx bit (e.g. same as read command)
+
+``` 
+/1/dWrt 2,LOW
+{"PD2":"LOW"}
+/1/dWrt 3,HIGH
+{"PD3":"LOW"}
+```
+
+Digital 3 is set as INPUT so it is not in the push-pull mode, the HIGH turns on a weak pullup and a LOW turns off the pullup.
+
+
+##  /0/dTog 2|3|24..27
+
+Toggle the Port Data Register (PORTx) bit if the Data Direction Register (DDRx) bit is set as an OUTPUT. Returns the Port Input Register PINx bit (e.g. same as read command)
+
+``` 
+/1/dTog 2
+{"PD2":"HIGH"}
+/1/dTog 3
+{"PD3":"LOW"}
+```
+
+Toggle will ckeck if in OUTPUT mode befor changing it
+
+
+##  /0/dRe? 2|3|24..27
+
+Read the Port Input Register (PINx) bit that was latched during last low edge of the system clock.
+
+``` 
+/1/dRe? 2
+{"PD2":"HIGH"}
+/1/dRe? 3
+{"PD3":"LOW"}
+```
---
+++ Digital/digital.c
@@ -0,0 +1,306 @@
+/*
+digital is part of Digital, it is a serial command interface to some digital Wiring like functions, 
+Copyright (C) 2016 Ronald Sutherland
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+For a copy of the GNU General Public License use
+http://www.gnu.org/licenses/gpl-2.0.html
+*/
+#include <avr/pgmspace.h>
+#include <util/atomic.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include "../lib/parse.h"
+#include "../lib/timers.h"
+#include "../lib/pin_num.h"
+#include "../lib/pins_board.h"
+#include "digital.h"
+
+#define SERIAL_PRINT_DELAY_MILSEC 10000
+static unsigned long serial_print_started_at;
+
+// pin number must be valid in arg[0] from parse
+void echo_digital_pin_in_json_rply(void)
+{
+    if (atoi(arg[0]) == 2) 
+    {
+        printf_P(PSTR("PD2"));
+    }
+    if (atoi(arg[0]) == 3) 
+    {
+        printf_P(PSTR("PD3"));
+    }
+    if (atoi(arg[0]) == 24)
+    {
+        printf_P(PSTR("PA0"));
+    }
+    if (atoi(arg[0]) == 25)
+    {
+        printf_P(PSTR("PA1"));
+    }
+    if (atoi(arg[0]) == 26)
+    {
+        printf_P(PSTR("PA2"));
+    }
+    if (atoi(arg[0]) == 27)
+    {
+        printf_P(PSTR("PA3"));
+    }
+}
+
+// pinMode( arg[0], arg[1] )
+void Mode(void)
+{
+    if ( (command_done == 10) )
+    {
+        // check that arg[0] is a digit 
+        if ( ( !( isdigit(arg[0][0]) ) ) )
+        {
+            printf_P(PSTR("{\"err\":\"pModeNaN\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        // and arg[0] value is 2|3|24|25|26|27 
+        uint8_t a = atoi(arg[0]);
+        if ( (a != 2) && (a != 3) && ( ( a < 24) || (a > 27) ) )
+        {
+            printf_P(PSTR("{\"err\":\"pModeOutOfRng\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        // also arg[1] is not ('INPUT' or 'OUTPUT')
+        if ( !( (strcmp_P( arg[1], PSTR("INPUT")) == 0) || (strcmp_P( arg[1], PSTR("OUTPUT")) == 0) ) ) 
+        {
+            printf_P(PSTR("{\"err\":\"pModeNaMode\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        serial_print_started_at = millis();
+        if (strcmp_P( arg[1], PSTR("OUTPUT")) == 0 ) 
+        {
+            pinMode(a, OUTPUT);
+        }
+        else
+        {
+            pinMode(a, INPUT);
+        }
+        
+        printf_P(PSTR("{\""));
+        command_done = 11;
+    }
+    else if ( (command_done == 11) )
+    {  
+        echo_digital_pin_in_json_rply();
+        printf_P(PSTR("\":\""));
+        command_done = 12;
+    }
+    else if ( (command_done == 12) )
+    {
+        printf( arg[1] );
+        printf_P(PSTR("\"}\r\n"));
+        initCommandBuffer();
+    }
+    else
+    {
+        printf_P(PSTR("{\"err\":\"pModeCmdDnWTF\"}\r\n"));
+        initCommandBuffer();
+    }
+}
+
+// digitalWrite( arg[0], arg[1] )
+void Write(void)
+{
+    if ( (command_done == 10) )
+    {
+        // check that arg[0] is a digit 
+        if ( ( !( isdigit(arg[0][0]) ) ) )
+        {
+            printf_P(PSTR("{\"err\":\"dWrtNaN\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        // and arg[0] value is 2|3|24|25|26|27 
+        uint8_t a = atoi(arg[0]);
+        if ( (a != 2) && (a != 3) && ( ( a < 24) || (a > 27) ) )
+        {
+            printf_P(PSTR("{\"err\":\"dWrtOutOfRng\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        // also arg[1] is not ('HIGH' or 'LOW')
+        if ( !( (strcmp_P( arg[1], PSTR("HIGH")) == 0) || (strcmp_P( arg[1], PSTR("LOW")) == 0) ) ) 
+        {
+            printf_P(PSTR("{\"err\":\"dWrtNaState\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        serial_print_started_at = millis();
+        if (strcmp_P( arg[1], PSTR("HIGH")) == 0 ) 
+        {
+            digitalWrite(a, HIGH);
+        }
+        else
+        {
+            digitalWrite(a, LOW);
+        }
+        
+        printf_P(PSTR("{\""));
+        command_done = 11;
+    }
+    else if ( (command_done == 11) )
+    {  
+        echo_digital_pin_in_json_rply();
+        printf_P(PSTR("\":\""));
+        command_done = 12;
+    }
+    else if ( (command_done == 12) )
+    {
+        uint8_t a = atoi(arg[0]);
+        if ( (a > (NUM_DIGITAL_PINS-1)) ) // the badPinCheck will barf at compile time without testing the value ... amazing
+        {
+            return;
+        }
+        bool pin = digitalRead(a);
+        if (pin)
+        {
+            printf_P(PSTR("HIGH"));
+        }
+        else
+        {
+            printf_P(PSTR("LOW"));
+        }
+        printf_P(PSTR("\"}\r\n"));
+        initCommandBuffer();
+    }
+    else
+    {
+        printf_P(PSTR("{\"err\":\"dWrtCmdDnWTF\"}\r\n"));
+        initCommandBuffer();
+    }
+}
+
+// digitalToggle( arg[0] )
+void Toggle(void)
+{
+    if ( (command_done == 10) )
+    {
+        // check that arg[0] is a digit 
+        if ( ( !( isdigit(arg[0][0]) ) ) )
+        {
+            printf_P(PSTR("{\"err\":\"dTogNaN\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        // and arg[0] value is 2|3|24|25|26|27  
+        uint8_t a = atoi(arg[0]);
+        if ( (a != 2) && (a != 3) && ( ( a < 24) || (a > 27) ) )
+        {
+            printf_P(PSTR("{\"err\":\"dTogOutOfRng\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        serial_print_started_at = millis();
+        digitalToggle(a);
+        
+        printf_P(PSTR("{\""));
+        command_done = 11;
+    }
+    else if ( (command_done == 11) )
+    {  
+        echo_digital_pin_in_json_rply();
+        printf_P(PSTR("\":\""));
+        command_done = 12;
+    }
+    else if ( (command_done == 12) )
+    {
+        uint8_t a = atoi(arg[0]);
+        if ( (a > (NUM_DIGITAL_PINS-1)) ) // the badPinCheck will barf at compile time without testing the value ... amazing
+        {
+            return;
+        }
+        bool pin = digitalRead(a);
+        if (pin)
+        {
+            printf_P(PSTR("HIGH"));
+        }
+        else
+        {
+            printf_P(PSTR("LOW"));
+        }
+        printf_P(PSTR("\"}\r\n"));
+        initCommandBuffer();
+    }
+    else
+    {
+        printf_P(PSTR("{\"err\":\"dTogCmdDnWTF\"}\r\n"));
+        initCommandBuffer();
+    }
+}
+
+//digitalRead( arg[0] )
+void Read(void)
+{
+    if ( (command_done == 10) )
+    {
+        // check that arg[0] is a digit 
+        if ( ( !( isdigit(arg[0][0]) ) ) )
+        {
+            printf_P(PSTR("{\"err\":\"dRdNaN\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        // and arg[0] value is 2|3|24|25|26|27 
+        uint8_t a = atoi(arg[0]);
+        if ( (a != 2) && (a != 3) && ( ( a < 24) || (a > 27) ) )
+        {
+            printf_P(PSTR("{\"err\":\"dRdOutOfRng\"}\r\n"));
+            initCommandBuffer();
+            return;
+        }
+        serial_print_started_at = millis();
+        
+        printf_P(PSTR("{\""));
+        command_done = 11;
+    }
+    else if ( (command_done == 11) )
+    {  
+        echo_digital_pin_in_json_rply();
+        printf_P(PSTR("\":\""));
+        command_done = 12;
+    }
+    else if ( (command_done == 12) )
+    {
+        uint8_t a = atoi(arg[0]);
+        if ( (a > (NUM_DIGITAL_PINS-1)) ) // the badPinCheck will barf at compile time without testing the value ... amazing
+        {
+            return;
+        }
+        bool pin = digitalRead(a);
+        if (pin)
+        {
+            printf_P(PSTR("HIGH"));
+        }
+        else
+        {
+            printf_P(PSTR("LOW"));
+        }
+        printf_P(PSTR("\"}\r\n"));
+        initCommandBuffer();
+    }
+    else
+    {
+        printf_P(PSTR("{\"err\":\"dRdCmdDnWTF\"}\r\n"));
+        initCommandBuffer();
+    }
+}
